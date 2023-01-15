01871239e41ecdf90c132b4a49b08a7c3814f209
Igor Cassolli <37945750+IgorCassolli@users.noreply.github.com>
2023-01-15 20:30:42 -0300

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar } from "./components/Sidebar"
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https:github.com/IgorCassolli.png',
+      name: 'Igor Cassolli',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare'},          
+    ],
+    publishedAt: new Date('2022-12-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https:github.com/IgorCassolli.png',
+      name: 'Natalia Arruda',
+      role: 'Psychologist'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Testando', },
+      { type: 'link', content: 'jane.design/doctorcare'},          
+    ],
+    publishedAt: new Date('2023-01-01 20:00:00'),
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -12,11 +43,18 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post 
-          author="Igor da Silva Cassolli"
-          content="Isso é um teste, vamos lá, dale Grêmio, é us guri, vamo Grêmio FI FI FI"
-        />
-
+          {
+            posts.map(post => {
+              return (
+                <Post  
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </div>
